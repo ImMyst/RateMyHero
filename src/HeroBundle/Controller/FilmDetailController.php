2d1b4acdf5a9e59d948aee96a1986bda37a4ad1f
Vagrant Default User <vagrant@stretch.localdomain>
2017-10-18 13:49:22 +0000

    Changements style et autre pour pres

---
+++ src/HeroBundle/Controller/FilmDetailController.php
@@ -0,0 +1,30 @@
+<?php
+namespace HeroBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use HeroBundle\Repository\FilmRepository;
+use HeroBundle\Repository\HeroRepository;
+use HeroBundle\Controller\FilmDetailController;
+use HeroBundle\Entity\Hero;
+use HeroBundle\Entity\Film;
+
+
+class FilmDetailController extends Controller
+{
+    /**
+     * @Route("/film/detail", name="film_detail")
+     */
+    public function detailAction()
+    {
+
+        $filmRepository = $this->getDoctrine()->getRepository(Film::class);
+        $film = $filmRepository->findAll();
+
+        return $this->render('HeroBundle:Default:filmdetail.html.twig', [
+            'film' => $film,
+        ]);
+    }
+}
